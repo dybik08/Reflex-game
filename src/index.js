af7f5b838cc919f82e04fcc73248a660142b9e61
dybik08 <dybik08@gmail.com>
2019-01-15 18:44:09 +0100

    [NEW FEATURE] Create function that reset current game and hook it up with restart game button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 window.onload = () => {
     //init variables
+    //dom ones
+    const gameArea = document.getElementsByClassName('game-area');
+    const startButton = document.getElementById('startBtn');
+    const stopButton = document.getElementById('stopBtn');
+    const timerDisplayer = document.getElementById('timer');
+    const pointsDisplayer = document.getElementById('timer');
+    const livesDisplayer = document.getElementById('lives');
+
+    //game ones
     const squareNumbers = 24;
     let gameTimer = 5;
     let lives = 3;
-    const gameArea = document.getElementsByClassName('game-area');
-    const startButton = document.getElementById('startBtn');
-    const timer = document.getElementById('timer');
-    timer.innerText = `Pozostały czas: ${gameTimer} sek`;
-    let timerWorking = false;
+    let gameWorking = false;
+    let timerInterval;
 
   //draw game area
     const drawGameArea = () => {
@@ -19,22 +25,35 @@ window.onload = () => {
         }
     };
 
-
-    //start game
-    startButton.onclick = () => {
-        if(!timerWorking){
-            let timerInterval = setInterval(()=>{
+    //start game fn
+    const startGame = (e, gameWorking = false, gameTimer = 7) => {
+        console.log(gameTimer, gameWorking)
+        if(!gameWorking){
+            timerInterval = setInterval(()=>{
                 if(gameTimer>=1){
                     gameTimer -=1;
-                    timer.innerText = `Pozostały czas: ${gameTimer} sek`;
+                    timerDisplayer.innerText = `Pozostały czas: ${gameTimer} sek`;
                 }else {
-                    clearInterval(timerInterval)
+                    clearInterval(timerInterval);
                     return alert("'Time's up!")
                 }
             },1000);
-            timerWorking = true;
         }
     };
+
+    //start game btn
+    startButton.onclick = startGame;
+
+    //restart game btn
+    stopButton.onclick = () => {
+        clearInterval(timerInterval)
+        gameTimer = 10;
+        timerDisplayer.innerText = `Pozostały czas: ${gameTimer} sek`;
+        lives = 2;
+        livesDisplayer.innerText = `Życia: ${lives}`;
+        gameWorking = false;
+    };
+
 
     drawGameArea();
 };
